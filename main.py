f47d997006f8936b4b3f3b0acfef0e6469a62db3
dayna_l <dayna.lashin@gmail.com>
2021-03-10 00:30:30 -0500

    Made the program loop until you select exit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import turtle as turtle
 import random
 
 random_number = random.randrange(1,5)
+done = False
 
 print("Welcome to your drawing app! Select a shape you want to draw. After typing the number and hitting enter, go to the result tab!")
 
@@ -27,56 +28,76 @@ turtle.left(90)
 turtle.forward(150)
 turtle.pendown()
 
-def star():
-  turtle.pencolor("purple")
-  for i in range(0,5):
-    turtle.forward(110)
-    turtle.right(216)
 
-def square():
-  turtle.pencolor("orange")
-  for i in range(0,4):
+while done == False:
+  def star():
+    turtle.pencolor("purple")
+    for i in range(0,5):
+      turtle.forward(110)
+      turtle.right(216)
+
+  def square():
+    turtle.pencolor("orange")
+    for i in range(0,4):
       turtle.forward(100)
       turtle.right(90)
 
-def hexagon():
-  turtle.pencolor("red")
-  for i in range(0,6):
-    turtle.forward(100)
-    turtle.right(60)
+  def hexagon():
+    turtle.pencolor("red")
+    for i in range(0,6):
+      turtle.forward(100)
+      turtle.right(60)
 
-def triangle():
-  turtle.pencolor("green")
-  for i in range(0,3):
-    turtle.forward(110)
-    turtle.right(120)
+  def triangle():
+    turtle.pencolor("green")
+    for i in range(0,3):
+      turtle.forward(110)
+      turtle.right(120)
 
-def random():
-  turtle.pencolor("black")
-  if random_number == 1:
+  def random():
+    turtle.pencolor("black")
+    if random_number == 1:
+      star()
+    elif random_number == 2:
+      square()
+    elif random_number == 3:
+      hexagon()
+    elif random_number == 4:
+      triangle()
+
+
+  selection = input("\n1. Star\n2. Square\n3. Hexagon\n4. Triangle\n5. Random\n6. Exit\nSelect a number: ")
+
+  if selection == "1":
+    turtle.clear()
+    print("Excellent choice! Go to the result tab to see your creation.")
     star()
-  elif random_number == 2:
+    print("\nPick again!")
+
+  elif selection == "2":
+    turtle.clear()
+    print("Excellent choice! Go to the result tab to see your creation.")
     square()
-  elif random_number == 3:
+    print("\nPick again!")
+
+  elif selection == "3":
+    turtle.clear()
+    print("Excellent choice! Go to the result tab to see your creation.")
     hexagon()
-  elif random_number == 4:
+    print("\nPick again!")
+
+  elif selection == "4":
+    turtle.clear()
+    print("Excellent choice! Go to the result tab to see your creation.")
     triangle()
+    print("\nPick again!")
 
+  elif selection == "5":
+    turtle.clear()
+    print("Random Shape, coming right up! Go to the result tab to see your creation.")
+    random()
+    print("\nPick again!")
 
-selection = input("1. Star\n2. Square\n3. Hexagon\n4. Triangle\n5. Random\nSelect a number: ")
-if selection == "1":
-  print("Excellent choice! Go to the result tab to see your creation.")
-  star()
-elif selection == "2":
-  print("Excellent choice! Go to the result tab to see your creation.")
-  square()
-elif selection == "3":
-  print("Excellent choice! Go to the result tab to see your creation.")
-  hexagon()
-elif selection == "4":
-  print("Excellent choice! Go to the result tab to see your creation.")
-  triangle()
-elif selection == "5":
-  print("Random Shape, coming right up! Go to the result tab to see your creation.")
-  print(random_number)
-  random()
+  elif selection == "6":
+    print("\nThanks for playing!\n")
+    done = True
